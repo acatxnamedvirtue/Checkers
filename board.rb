457a769b95f1d9e60d346784b64347cff89a876b
acatxnamedvirtue <tsb252@gmail.com>
2015-08-06 11:20:14 -0400

    Define Board#render, Board#add_black_pieces, Board#add_white_pieces, Board#[], Board#[]=, Board#setup_board, Piece#to_s, and unicode for Piece.

--- board.rb
+++ board.rb
@@ -1,10 +1,86 @@
+require_relative 'piece'
+
 class Board
   SIZE = 8
 
-  def initialize
+  def initialize(setup = true)
     @grid = Array.new(SIZE) { Array.new(SIZE) }
+    setup_board if setup
   end
 
+  def setup_board
+    #0,2 #1
+    add_white_pieces
+    #5,7 #6
+    add_black_pieces
+  end
+
+
+
   def render
+    puts "   0 1 2 3 4 5 6 7"
+    puts "   _______________"
+    rows.each_with_index do |row, row_index|
+      print "#{row_index} |"
+      row.each do |tile|
+        if tile.nil?
+          print "_|"
+        else
+          print "#{tile.to_s}|"
+        end
+        # tile.nil? ? (print "_ ") : (print tile.to_s)
+      end
+      puts
+    end
+  end
+
+  def rows
+    @grid
+  end
+
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    @grid[row][col] = value
+  end
+
+  def add_white_pieces
+    rows.each_with_index do |row, row_index|
+      next if row_index > 2
+      row.each_with_index do |_, col_index|
+        case row_index.even?
+        when true # even rows
+          next if col_index.even?
+          self[[row_index,col_index]] = Piece.new(:white, [row_index, col_index])
+        when false # odd rows
+          next if col_index.odd?
+          self[[row_index,col_index]] = Piece.new(:white, [row_index, col_index])
+        end
+      end
+    end
+  end
+
+  def add_black_pieces
+    rows.map.with_index do |row, row_index|
+      next if row_index < 5
+      row.map.with_index do |_, col_index|
+        case row_index.even?
+        when true # even rows
+          next if col_index.even?
+          self[[row_index,col_index]] = Piece.new(:black, [row_index, col_index])
+        when false # odd rows
+          next if col_index.odd?
+          self[[row_index,col_index]] = Piece.new(:black, [row_index, col_index])
+        end
+      end
+    end
   end
 end
+
+board = Board.new
+# p board.rows
+board.render
--- piece.rb
+++ piece.rb
@@ -1,6 +1,22 @@
 class Piece
-  def initialize
+
+  UNICODE = {
+    white_pawn: "\u2659",
+    white_king: "\u2654",
+    black_pawn: "\u265F",
+    black_king: "\u265A"
+  }
+
+  attr_reader :color
+
+  def initialize(color, pos)
+    @color = color
     @king = false
+    @pos = pos
+  end
+
+  def king?
+    @king
   end
 
   def perform_slide
@@ -14,5 +30,22 @@ class Piece
 
   def maybe_promote
   end
-  
+
+  def to_s
+    case color
+    when :white
+      if king?
+        return UNICODE[:white_king].encode
+      else
+        return UNICODE[:white_pawn].encode
+      end
+    when :black
+      if king?
+        return UNICODE[:black_king].encode
+      else
+        return UNICODE[:black_pawn].encode
+      end
+    end
+  end
+
 end
