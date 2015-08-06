87b1e8e448e0c4f255a62e8990cb869f77d211c2
acatxnamedvirtue <tsb252@gmail.com>
2015-08-06 10:14:54 -0400

    Create Board and Piece classes

---
+++ board.rb
@@ -0,0 +1,10 @@
+class Board
+  SIZE = 8
+
+  def initialize
+    @grid = Array.new(SIZE) { Array.new(SIZE) }
+  end
+
+  def render
+  end
+end
---
+++ piece.rb
@@ -0,0 +1,18 @@
+class Piece
+  def initialize
+    @king = false
+  end
+
+  def perform_slide
+  end
+
+  def perform_jump
+  end
+
+  def move_diffs
+  end
+
+  def maybe_promote
+  end
+  
+end
