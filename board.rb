0118441fb058d1be81a037d96416ff6d1911892e
acatxnamedvirtue <tsb252@gmail.com>
2015-08-06 14:47:31 -0400

    refactor

--- board.rb
+++ board.rb
@@ -9,15 +9,6 @@ class Board
     setup_board if setup
   end
 
-  def setup_board
-    #0,2 #1
-    add_white_pieces
-    #5,7 #6
-    add_black_pieces
-  end
-
-
-
   def render
     system("clear")
     puts "    0  1  2  3  4  5  6  7"
@@ -35,29 +26,6 @@ class Board
     end
   end
 
-  def tile_color(row, col)
-    case row.even?
-    when true #rows 0,2,4,6
-      case col.even?
-      when true
-        :light_white
-      when false
-        :red
-      end
-    when false #rows 1,3,5,7
-      case col.even?
-      when true
-        :red
-      when false
-        :light_white
-      end
-    end
-  end
-
-  def rows
-    @grid
-  end
-
   def [](pos)
     row, col = pos
     @grid[row][col]
@@ -68,40 +36,54 @@ class Board
     @grid[row][col] = value
   end
 
-  def move(start_pos, move_pos)
-    self[start_pos].move(move_pos)
+  def move(start_pos, end_pos)
+    piece = self[start_pos]
+    if piece.slide_moves.include?(end_pos)
+      piece.perform_slide(end_pos)
+    elsif piece.jump_moves.include?(end_pos)
+      piece.perform_jump(end_pos)
+    else
+      puts "Not a valid move!"
+    end
   end
 
-  def add_white_pieces
-    rows.each_with_index do |row, row_index|
-      next if row_index > 2
-      row.each_with_index do |_, col_index|
-        case row_index.even?
+  private
+  def setup_board
+    add_pieces(:white)
+    add_pieces(:black)
+  end
+
+  def tile_color(row, col)
+    case row.even?
+    when true #rows 0,2,4,6
+      col.even? ? :light_white : :red
+    when false #rows 1,3,5,7
+      col.even? ? :red : :light_white
+    end
+  end
+
+  def color_setter(color)
+    return (0..2) if color == :white
+    return (5..(SIZE - 1)) if color == :black
+  end
+
+  def add_pieces(color)
+    color_setter(color).each do |row|
+      (0..(SIZE - 1)).each do |col|
+        case row.even?
         when true # even rows
-          next if col_index.even?
-          self[[row_index,col_index]] = Piece.new(self, :white, [row_index, col_index])
+          next if col.even?
+          self[[row,col]] = Piece.new(self, color, [row, col])
         when false # odd rows
-          next if col_index.odd?
-          self[[row_index,col_index]] = Piece.new(self, :white, [row_index, col_index])
+          next if col.odd?
+          self[[row,col]] = Piece.new(self, color, [row, col])
         end
       end
     end
   end
 
-  def add_black_pieces
-    rows.map.with_index do |row, row_index|
-      next if row_index < 5
-      row.map.with_index do |_, col_index|
-        case row_index.even?
-        when true # even rows
-          next if col_index.even?
-          self[[row_index,col_index]] = Piece.new(self, :black, [row_index, col_index])
-        when false # odd rows
-          next if col_index.odd?
-          self[[row_index,col_index]] = Piece.new(self, :black, [row_index, col_index])
-        end
-      end
-    end
+  def rows
+    @grid
   end
 end
 
--- piece.rb
+++ piece.rb
@@ -41,57 +41,11 @@ class Piece
     @pos = pos
   end
 
-  def jump_diffs
-    JUMP_DIFFS
-  end
-
-  def slide_diffs
-    SLIDE_DIFFS
-  end
-
-  def king?
-    @rank == :king
-  end
-
-  def promote_king
-    @rank = :king
-  end
-
-  def slide_moves
-    slide_moves = []
-
-    slide_diffs[color][rank].each do |d_pos|
-      new_move = calculate_new_move(d_pos)
-      slide_moves << new_move if valid_move?(new_move)
-    end
-
-    slide_moves.sort
-  end
-
   def perform_slide(move_pos)
     return false unless slide_moves.include?(move_pos)
 
     move(move_pos)
     maybe_promote
-  end
-
-  def jump_moves
-    jump_moves = []
-
-    jump_diffs[color][rank].each do |d_pos|
-      new_move = calculate_new_move(d_pos)
-      opponent_pos = calculate_opponent_pos(d_pos)
-
-      jump_moves << new_move if valid_move?(new_move) && opponent_piece?(opponent_pos)
-    end
-
-    jump_moves
-  end
-
-  def opponent_piece?(opponent_pos)
-    return false if board[opponent_pos].nil? || board[opponent_pos].color == color
-
-    true
   end
 
   def perform_jump(move_pos)
@@ -100,56 +54,6 @@ class Piece
     delete_piece(self.pos, move_pos)
     move(move_pos)
     maybe_promote
-  end
-
-  def delete_piece(start_pos, move_pos)
-    start_row, start_col = start_pos
-    move_row, move_col = move_pos
-    delete_row = start_row - (start_row - move_row)/2
-    delete_col = start_col - (start_col - move_col)/2
-
-    board[[delete_row, delete_col]] = nil
-  end
-
-  def move(move_pos)
-    board[move_pos] = self
-    board[self.pos] = nil
-    self.pos = move_pos
-  end
-
-  def calculate_new_move(d_pos)
-    orig_row, orig_col = pos
-    d_row, d_col = d_pos
-    new_row = orig_row + d_row
-    new_col = orig_col + d_col
-
-    [new_row, new_col]
-  end
-
-  def calculate_opponent_pos(d_pos)
-    orig_row, orig_col = pos
-    d_row, d_col = d_pos
-    new_row = orig_row + (d_row / 2)
-    new_col = orig_col + (d_col / 2)
-
-    [new_row, new_col]
-  end
-
-  def valid_move?(move_pos)
-      board[move_pos].nil? && in_bounds?(move_pos)
-  end
-
-  def in_bounds?(move_pos)
-    move_pos.all? { |coord| coord.between?(0,7) }
-  end
-
-  def maybe_promote
-    case color
-    when :black
-      promote_king if self.pos.first == 0
-    when :white
-      promote_king if self.pos.first == 7
-    end
   end
 
   def to_s
@@ -167,5 +71,96 @@ class Piece
         UNICODE[:black_pawn].encode
       end
     end
+  end
+
+  # private
+  def jump_diffs
+    JUMP_DIFFS
+  end
+
+  def slide_diffs
+    SLIDE_DIFFS
+  end
+
+  def slide_moves
+    slide_diffs[color][rank].map { |d_pos| calc_new_move(d_pos)}.select{|move| valid_move?(move)}
+  end
+
+  def jump_moves
+    jump_moves = []
+
+    jump_diffs[color][rank].each do |d_pos|
+      new_move = calc_new_move(d_pos)
+      opponent_pos = calc_opponent_pos(d_pos)
+
+      jump_moves << new_move if valid_move?(new_move) && opponent_piece?(opponent_pos)
+    end
+
+    jump_moves
+  end
+
+  def opponent_piece?(opponent_pos)
+    return false if board[opponent_pos].nil? || board[opponent_pos].color == color
+
+    true
+  end
+
+  def delete_piece(start_pos, move_pos)
+    start_row, start_col = start_pos
+    move_row, move_col = move_pos
+    delete_row = start_row - (start_row - move_row)/2
+    delete_col = start_col - (start_col - move_col)/2
+
+    board[[delete_row, delete_col]] = nil
+  end
+
+  def move(move_pos)
+    board[move_pos] = self
+    board[self.pos] = nil
+    self.pos = move_pos
+  end
+
+  def calc_new_move(d_pos)
+    orig_row, orig_col = pos
+    d_row, d_col = d_pos
+    new_row = orig_row + d_row
+    new_col = orig_col + d_col
+
+    [new_row, new_col]
+  end
+
+  def calc_opponent_pos(d_pos)
+    orig_row, orig_col = pos
+    d_row, d_col = d_pos
+    new_row = orig_row + (d_row / 2)
+    new_col = orig_col + (d_col / 2)
+
+    [new_row, new_col]
+  end
+
+  def valid_move?(move_pos)
+      board[move_pos].nil? && in_bounds?(move_pos)
+  end
+
+  def in_bounds?(move_pos)
+    move_pos.all? { |coord| coord.between?(0, Board::SIZE - 1) }
+  end
+
+  def maybe_promote
+    return if self.king?
+    case color
+    when :black
+      promote_king if self.pos.first == 0
+    when :white
+      promote_king if self.pos.first == Board::SIZE - 1
+    end
+  end
+
+  def promote_king
+    @rank = :king
+  end
+
+  def king?
+    @rank == :king
   end
 end
